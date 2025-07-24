5df9b2f0c6ea5e2620508e938a2b1d0865abf301
umamaheshdega@umadega <mahesh445667@gmail.com>
2025-07-24 11:54:39 +0530

    backend

--- Backend/index.js
+++ Backend/index.js
@@ -17,8 +17,11 @@ app.use(cors());
 // =======================================================
 
 //Health Checking
-app.get('/health',(req,res)=>{
-    res.json("This is the health check for blue-3.0.0");
+// app.get('/health',(req,res)=>{
+//     res.json("This is the health check for blue-3.0.0");
+// });
+app.get("/", (req, res) => {
+  res.send("App is working!");
 });
 
 // ADD TRANSACTION
